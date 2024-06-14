43f6709ee05e3afd0375062950ad4be147462efb
Memin Seeker <72305188+Meminseeker@users.noreply.github.com>
2024-06-14 21:51:53 +0300

    chore: update JobListings component to use json-server as data source

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,8 +1,28 @@
-import jobs from "../jobs.json";
+import { useState, useEffect } from "react";
 import JobListing from "./JobListing";
 
 const JobListings = ({ isHome = false }) => {
-	const jobListings = isHome ? jobs.jobs.slice(0, 3) : jobs.jobs;
+	const [jobs, setJobs] = useState([]);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		const fetchJobs = async () => {
+			try {
+				const apiUrl = isHome
+					? "http://localhost:8000/jobs?_limit=3"
+					: "http://localhost:8000/jobs";
+				const res = await fetch(apiUrl);
+				const data = await res.json();
+				setJobs(data);
+			} catch (error) {
+				console.log("Error fetching data", error);
+			} finally {
+				setLoading(false);
+			}
+		};
+
+		fetchJobs();
+	}, []);
 
 	return (
 		<section className="bg-blue-50 px-4 py-10">
@@ -11,9 +31,15 @@ const JobListings = ({ isHome = false }) => {
 					{isHome ? "Recent Jobs" : "Browse Jobs"}
 				</h2>
 				<div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-					{jobListings.map((job) => (
-						<JobListing key={job.id} job={job} />
-					))}
+					{loading ? (
+						<h2>Loading...</h2>
+					) : (
+						<>
+							{jobs.map((job) => (
+								<JobListing key={job.id} job={job} />
+							))}
+						</>
+					)}
 				</div>
 			</div>
 		</section>
